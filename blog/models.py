54d8dbb9852e4a8e0e1973688facbc4557c14d30
itsmexta777 <kenniamexta@gmail.com>
2026-03-23 17:38:01 -0700

    guardar cosas de hoy 23

--- blog/models.py
+++ blog/models.py
@@ -1,38 +1,38 @@
 from django.db import models
 
-# Create your models here.
-
 class Category(models.Model):
     title = models.CharField(max_length=225)
 
-    class meta:
-        ordering = ('title')
-        verbose_name_plural = 'catefories'
+    class Meta:
+        ordering = ('title',)
+        verbose_name_plural = 'categories'
 
     def __str__(self):
         return self.title
     
+
 class Post(models.Model):
 
     ACTIVE = 'active'
     DRAFT = 'draft'
 
-    CHOICES_STATUS = {
+    CHOICES_STATUS = [
         (ACTIVE, 'Active'),
         (DRAFT, 'Draft')
-    }
+    ]
 
-    category = models.ForeignKey(Category, related_name='post', on_delete=models.CASCADE)
-    tile = models.CharField(max_length=225)
+    category = models.ForeignKey(Category, related_name='posts', on_delete=models.CASCADE)
+    title = models.CharField(max_length=225)
     intro = models.TextField()
     body = models.TextField()
-    crated_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=10, choices=CHOICES_STATUS, default=ACTIVE)
     image = models.ImageField(upload_to='upload/', blank=True, null=True)
 
     def __str__(self):
-        return self.tile
+        return self.title
     
+
 class Comment(models.Model):
     post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
     name = models.CharField(max_length=225)
